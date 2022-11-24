2e55960b48bfaf7258bf3984f14058b610123e95
MythosOne <107143884+MythosOne@users.noreply.github.com>
2022-11-24 17:16:19 +0200

    goit-js-hw-10-1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,17 @@
+import Notiflix from 'notiflix';
+
 const BASE_URL = 'https://restcountries.com/v2';
 const API_KEY = 'name,capital,population,flags,languages';
 
 export default function fetchCountries(name) {
-    return fetch(`${BASE_URL}/name/${name}?fields=${API_KEY}`).then(resp => {
-        // console.log(resp);
+    return fetch(`${BASE_URL}/name/${name}?fields=${API_KEY}`)
+        .then(resp => {
+        console.log(resp);
         if (!resp.ok) {
-            throw new Error(resp.statusText);
+            Notiflix.Notify.failure(`Oops, there is no country with that name`);
+            // throw new Error(resp.statusText);
         }
         return resp.json();
-    }).catch(err => console.error(err));
+    })
+        // .catch(err => console.error(err));
 }
--- src/index.js
+++ src/index.js
@@ -6,24 +6,38 @@ import fetchCountries from './fetchCountries.js'
 const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const card = document.querySelector('.country-info');
 input.addEventListener('input', debounce(OnSearch, DEBOUNCE_DELAY));
 
-const list = document.querySelector('.country-list');
-// const listCard = document.querySelector('.country-info');
+
 
 function OnSearch(event) {
+    // event.preventDefault();
     const SearchValue= event.target.value.trim();
 
     if (!SearchValue) {
         list.remove();
+        card.remove();
         return;
     }
-    fetchCountries(SearchValue).then(data => creatMarkup(data));
-    
+
+    fetchCountries(SearchValue).then(data => {
+        console.log(data.length);
+        if (data.length > 10) {
+            Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);
+        } else if (data.length > 1 & data.length <= 10) {
+            console.log('👌');
+            creatMarkupFound(data);
+        } else if (data.length === 1) {
+            list.remove();
+            creatMarkupCard(data);
+        }
+    });
 }
 
-function creatMarkup(arr) {
-    const markup = arr.map(({flags, name}) => `<li style='display:flex'>
+function creatMarkupFound(data) {
+    const markup = data.map(({ flags, name }) => `<li>
     <img src="${flags.svg}" width="40px" alt="${name}"/>
     <h2>${name}</h2>
     </li>`);
@@ -34,16 +48,16 @@ function creatMarkup(arr) {
     list.innerHTML = markup.join('');
 }
 
-// function creatMarkupCard(arr) {
-//     const markup = arr.map(({ flags, name, capital, population, languages }) =>
-//         `<div class="country-info"><img src="${flags.svg}" width="70px" alt="${name}" />
-//         <h1>${name}</h1>
-//         <ul>
-//         <li><span>Capital:${capital}</span></li>
-//         <li><span>Population:${population}</span></li>
-//         <li><span>Languages:${languages.map(({name})=> `<span>${name}</span>`).join(', ')}</span></li>
-//         </ul>
-//         </div>`
-//     );
-//     list.innerHTML = markup.join('');
-// }
+function creatMarkupCard(data) {
+    const markup = data.map(({ flags, name, capital, population, languages }) =>
+    `<div class="country-info"><img src="${flags.svg}" width="70px" alt="${name}" />
+    <h1>${name}</h1>
+    <ul>
+    <li><span>Capital:${capital}</span></li>
+    <li><span>Population:${population}</span></li>
+    <li><span>Languages:${languages.map(({name})=> `<span>${name}</span>`).join(', ')}</span></li>
+    </ul>
+    </div>`
+    );
+    card.innerHTML = markup.join('');
+}
